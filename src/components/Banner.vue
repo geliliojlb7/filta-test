e1aecb4f9a0c2026ba04cd4ba6232a02afbfc10d
geliliojlb7 <geliliojohnlloydb7@gmail.com>
2023-12-07 06:20:29 +0800

    finished filta exercise 1

---
+++ src/components/Banner.vue
@@ -0,0 +1,57 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="banner p-4">
+    <h1>{{ title }}</h1>
+    <h4>{{ details }}</h4>
+  </div>
+</template>
+
+<style scoped>
+.banner {
+  background-image: url('https://via.placeholder.com/600x600');
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: cover;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+  color: #fff;
+  text-align: center;
+	height: 600px;
+}
+
+h1 {
+  font-weight: 600;
+  font-size: 4.3rem;
+}
+
+h4 {
+	font-size: 2rem
+}
+
+@media (min-width: 768px) {
+	.banner {
+		margin: 0;
+		width: 100%;
+	}
+}
+
+@media (min-width: 1024px) {
+	.banner {
+		background-image: url('../assets/1920x650.png');
+	}
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script>
+import Banner from '../components/Banner.vue'
+import CardSection from '../components/CardSection.vue'
+
+export default {
+  components: {
+    Banner,
+    CardSection,
+  },
+  data() {
+    return {
+      cards: [
+        {
+          title: "Card 1",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        },
+        {
+          title: "Card 2",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+        {
+          title: "Card 3",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <Banner title="Hello Developer!" details="Lorem ipsum dolor sit amet, consectetur adipiscing elit"/>
+  <CardSection :cards="cards" />
+</template>
